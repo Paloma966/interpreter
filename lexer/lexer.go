90445b6a5f8159f0be70a85bfa708ba856d055f7
paloma966 <xiejunbo2580@163.com>
2026-04-12 17:50:40 +0800

    fix: fix lexer position bug and update test cases

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -11,21 +11,24 @@ type Lexer struct {
 
 func New(input string) *Lexer {
 	l := &Lexer{input: input}
-	l.readCher()
+	l.readChar()
 	return l
 }
 
-func (l *Lexer) readCher() {
+func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
 	} else {
 		l.ch = l.input[l.readPosition]
-		l.readPosition += 1
 	}
+	l.position = l.readPosition
+	l.readPosition += 1
 }
 
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -52,12 +55,16 @@ func (l *Lexer) NextToken() token.Token {
 			tok.Literal = l.readIdentifier()
 			tok.Type = token.LookupIdent(tok.Literal)
 			return tok
+		} else if isDigit(l.ch) {
+			tok.Type = token.INT
+			tok.Literal = l.readNumber()
+			return tok
 		} else {
 			tok = newToken(token.ILLEGAL, l.ch)
 		}
 	}
 
-	l.readCher()
+	l.readChar()
 	return tok
 }
 
@@ -68,7 +75,7 @@ func newToken(tokenType token.TokenType, ch byte) token.Token {
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 	for isLetter(l.ch) {
-		l.readCher()
+		l.readChar()
 	}
 
 	return l.input[position:l.position]
@@ -76,4 +83,24 @@ func (l *Lexer) readIdentifier() string {
 
 func isLetter(ch byte) bool {
 	return ch >= 'a' && ch <= 'z' || ch >= 'A' && ch <= 'Z' || ch == '_'
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) readNumber() string {
+	position := l.position
+
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+
+	return l.input[position:l.position]
+}
+
+func isDigit(ch byte) bool {
+	return ch >= '0' && ch <= '9'
 }
